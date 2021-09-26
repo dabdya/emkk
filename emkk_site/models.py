86d7ec45e2727d643cbe271e77d5e8bc3e6d8824
SergeyPishchulov <lcmrb26@bk.ru>
2021-09-26 15:06:29 +0500

    User, Review, Role, Document Models

--- emkk_site/models.py
+++ emkk_site/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    """Документы, прилагаемые к заявке"""
+    trip = models.ForeignKey(Trip, on_delete=models.SET_NULL)
+    physical_path = models.FilePathField(max_length=255)
+
+
+class User(models.Model):
+    """Все пользователи. Все те, кто зарегистрированы"""
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    role = models.ManyToManyField(Role)
+
+
+class UserExperience(models.Model):
+    """Опыт пользователя по каждому виду туризма ~ категории сложности[1..6]"""
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    tripType = models.ForeignKey(TripType, on_delete=models.SET_NULL)
+    difficultyCategory = models.IntegerField()
+
+
+class Review(models.Model):
+    """Рецензия. Выдается работниоком МКК на конкретную заявку"""
+    reviewer = models.ForeignKey(User, on_delete=models.SET_NULL)
+    trip = models.ForeignKey(Trip, on_delete=models.SET_NULL)
+    result = models.ForeignKey(TripStatus, on_delete=models.SET_NULL)
+    result_comment = models.TextField()
+
+
+class Role(models.Model):
+    """Роли в МКК. Секретарь/Рецензент/Выпускающий/Руководитель группы"""
+    name = models.CharField(max_length=255)
+    priorityLevel = models.IntegerField()  # TODO узнать, выстроены ли их права линейно
