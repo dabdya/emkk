40110d0c06d7ba997c9abc851fa9f41edbcb0bef
SergeyPishchulov <lcmrb26@bk.ru>
2021-09-26 17:31:29 +0500

    get_trip_by_id view

--- emkk_site/urls.py
+++ emkk_site/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('trips/', views.index, name='index'),
-    path('trips/<int:trip_id>/', views.index, name='index'),
+    path('trips/<int:trip_id>/', views.get_trip_by_id, name='trip_by_id'),
     path('trips/<int:trip_id>/<int:document_id>/', views.index, name='index'),
 ]
--- emkk_site/views.py
+++ emkk_site/views.py
@@ -1,10 +1,23 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 import random
 
+from emkk_site.models import Trip
+
 
 def index(request):
-    ans = random.randint(0,1)
+    ans = random.randint(0, 1)
     ans = "ДА!" if ans else "Нет :("
     return HttpResponse(f"""
     <h1>Можно ли пойти в поход? (пожалуйста)</h1>
     <h1>{ans}</h1>""")
+
+
+def get_trip_by_id(request, *args, **kwargs):
+    trip_id = kwargs['trip_id']
+    try:
+        trip = Trip.objects.get(pk=int(trip_id))
+    except ObjectDoesNotExist:
+        trip = None
+        return HttpResponse("<html><body>Trip is not Found.</body></html>") #TODO Заменить на шаблонизатор
+    return HttpResponse(f"<html><body>This is trip #{trip.pk}.</body></html>")
