6f57632111e6967ddc108137c9b090e60ab4a44c
Knyazzini <knyazzev.lgor@gmail.com>
2021-10-07 21:10:21 +0500

    upd: вывод заявок

---
+++ frontend/src/components/App.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Trips }  from "./Trips.js";
+import { Login } from "./Login.js";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+
+
+export default function App() {
+	return (
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/trips">Trips</Link>
+						</li>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Switch>
+					<Route path="/trips">
+						<Trips />
+					</Route>
+
+					<Route path="/login">
+						<Login />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// class App extends React.Component {
+
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {};
+// 	}
+
+// 	async componentDidMount() {
+// 		const gg = await fetch("http://localhost:8000/api/trips");
+// 		let json = await gg.json();
+// 		console.log(json);
+// 		this.setState({j: 'Посмотри в консоль, там пришел джейсончик, пока не знаю как его отрендерить :('});
+// 		}
+
+// 	render() {
+// 		return <h2>{ this.state.j}</h2>;
+// 	}
+// }
+
+// export default App;
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export class Login extends React.Component {
+
+	render() {
+		return <h2>{"Форма логина"}</h2>;
+	}
+}
