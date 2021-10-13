518844fa031aa38e03100f802ddc96be5c5b45d5
Knyazzini <knyazzev.lgor@gmail.com>
2021-10-14 00:53:28 +0500

    upd: refactor, loginPage, sessionStorage->localStorage

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import axios from 'axios';
 
@@ -10,56 +10,58 @@ import Registration from "./Registration";
 import PrivateRoute from '../utils/PrivateRoute';
 import PublicRoute from '../utils/PublicRoute';
 import { getToken, removeUserSession, setUserSession } from '../utils/Common';
+import { Trips } from './Trips';
 
-function App() {
-  const [authLoading, setAuthLoading] = useState(true);
+export default class App extends React.Component {
 
-  useEffect(() => {
-    const token = getToken();
-    if (!token) {
-      return;
-    }
+	authLoading = null;
 
-    let config = {
-      headers: {
-        Authorization: 'Token ' + token //the token is a variable which holds the token
-        }
-    };
-    axios.get(`http://localhost:8000/auth/user`, config).then(response => {
-      setUserSession(response.data.user.token, response.data.user.username);
-      setAuthLoading(false);
-    }).catch(error => {
-      removeUserSession();
-      setAuthLoading(false);
-    });
-  }, []);
+	componentDidMount(){
+		const token = getToken();
+		if (!token) {
+	  		return;
+		}
+		let config = {
+			headers: {
+			  Authorization: 'Token ' + token //the token is a variable which holds the token
+			  }
+		  };
 
-  if (authLoading && getToken()) {
-    return <div className="content">Checking Authentication...</div>
-  }
-
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <div>
-          <div className="header">
-            <NavLink exact activeClassName="active" to="/">Home</NavLink>
-            <NavLink activeClassName="active" to="/login">Login</NavLink><small>(Access without token)</small>
-            <NavLink activeClassName="active" to="/signup">Registration</NavLink><small>(Access without token)</small>
-            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink><small>(Access with token only)</small>
-          </div>
-          <div className="content">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <PublicRoute path="/login" component={Login} />
-              <PublicRoute path="/signup" component={Registration} />
-              <PrivateRoute path="/dashboard" component={Dashboard} />
-            </Switch>
-          </div>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+		axios.get(`http://localhost:8000/auth/user`, config).then(response => {
+			setUserSession(response.data.user.token, response.data.user.username);
+			this.setState({authLoading:true});
+		  }).catch(error => {
+			removeUserSession();
+			this.setState({authLoading:false});
+		  });
+	}
+	render(){
+		if (this.authLoading && getToken()) {
+			return <div className="content">Checking Authentication...</div>
+		} else {
+			return (
+				<div className="App">
+				  <BrowserRouter>
+					<div>
+					  <div className="header">
+						<NavLink exact activeClassName="active" to="/">Home</NavLink>
+						<NavLink activeClassName="active" to="/login">Login</NavLink><small>(Access without token)</small>
+						<NavLink activeClassName="active" to="/signup">Registration</NavLink><small>(Access without token)</small>
+						<NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink><small>(Access with token only)</small>
+					  </div>
+					  <div className="content">
+						<Switch>
+						  <Route exact path="/" component={Home} />
+						  <Route exact path="/trips" component={Trips} />
+						  <PublicRoute path="/login" component={Login} />
+						  <PublicRoute path="/signup" component={Registration} />
+						  <PrivateRoute path="/dashboard" component={Dashboard} />
+						</Switch>
+					  </div>
+					</div>
+				  </BrowserRouter>
+				</div>
+			  );
+		}
+	}
 }
-
-export default App;
--- frontend/src/utils/Common.js
+++ frontend/src/utils/Common.js
@@ -1,23 +1,23 @@
 // return the user data from the session storage
 export const getUser = () => {
-  const userStr = sessionStorage.getItem('user');
-  if (userStr) return JSON.parse(userStr);
-  else return null;
+	const userStr = localStorage.getItem('user');
+	if (userStr) return JSON.parse(userStr);
+	else return null;
 }
 
 // return the token from the session storage
 export const getToken = () => {
-  return sessionStorage.getItem('token') || null;
+	return localStorage.getItem('token') || null;
 }
 
 // remove the token and user from the session storage
 export const removeUserSession = () => {
-  sessionStorage.removeItem('token');
-  sessionStorage.removeItem('user');
+	localStorage.removeItem('token');
+	localStorage.removeItem('user');
 }
 
 // set the token and user from the session storage
 export const setUserSession = (token, user) => {
-  sessionStorage.setItem('token', token);
-  sessionStorage.setItem('user', JSON.stringify(user));
+	localStorage.setItem('token', token);
+	localStorage.setItem('user', JSON.stringify(user));
 }
