3401319e878abb8d803b698bcbb20c931a2f40cb
dabdya <iamdabdya@gmail.com>
2021-10-28 13:21:12 +0500

    Make test <trips_for_reviews> more readable

--- src/emkk_site/services.py
+++ src/emkk_site/services.py
@@ -18,13 +18,15 @@ def try_change_status_from_review_to_at_issuer(trip):
 
 
 def get_trips_available_for_reviews():
-    all_trips = Trip.objects.all()
+    trips = Trip.objects.all()
     trips_available_for_review = []
-    for trip in all_trips:
-        on_review_now = len(TripsOnReviewByUser.objects.filter(trip=trip))
-        existing_reviews_count = len(Review.objects.filter(trip=trip))
+
+    for trip in trips:
         needed_reviews_count = get_reviewers_count_by_difficulty(trip.difficulty_category)
-        if on_review_now + existing_reviews_count < needed_reviews_count:
+        in_work_reviews = len(TripsOnReviewByUser.objects.filter(trip=trip))
+        actual_reviews = len(Review.objects.filter(trip=trip))
+
+        if in_work_reviews + actual_reviews < needed_reviews_count:
             trips_available_for_review.append(trip)
 
     return trips_available_for_review
--- src/emkk_site/tests/test_features.py
+++ src/emkk_site/tests/test_features.py
@@ -1,59 +1,28 @@
 from django.test import TestCase, Client
 
+from src.emkk_site.tests.base import BaseTest
 from src.emkk_site.services import get_reviewers_count_by_difficulty
 from src.emkk_site.models import Trip, Review, TripKind, TripStatus, ReviewResult, TripsOnReviewByUser
-from src.jwt_auth.models import User
 
 from collections import defaultdict
 import random
 
 
-class BaseTest:
-    """Helpful functions for testing features"""
-
-    # noinspection PyMethodMayBeStatic
-    def create_leader(self, username="admin_test", email="admin-test@gmail.com"):
-        leader = User(
-            username=username, email=email,
-            first_name="Admin", last_name="Admin")
-        leader.set_password("adminpassword")
-        leader.save()
-        return leader
-
-    # noinspection PyMethodMayBeStatic
-    def generate_trips(self, leader, count):
-        """Generate and save {count} trips with difference difficulty"""
-        for i in range(count):
-            difficulty = random.randint(1, 6)
-            trip = Trip(
-                kind=TripKind.CYCLING, group_name="TestGroup",
-                difficulty_category=difficulty,
-                global_region="Russia", local_region="City",
-                participants_count=12, start_date='2021-10-08',
-                end_date='2021-10-28', coordinator_info="Info",
-                insurance_info="Info", leader=leader)
-            trip.save()
-
-
 class TripsForReviewTest(TestCase, BaseTest):
-    """Tests check available trips for reviewers.
-    Create temporary database, after remove"""
+    """Tests check available trips for reviewers"""
 
     def setUp(self):
-        self.iters = 100
+        self.trips_count = 100
         self.client = Client()
         self.leader = self.create_leader()
-        self.generate_trips(self.leader, self.iters)
+        self.generate_trips(self.leader, self.trips_count)
 
     def test_trips_with_needed_reviews_count_should_filtered(self):
 
         trips = Trip.objects.all()
-        actual_reviews = defaultdict(int)
 
-        should_filtered = 0
-
-        """Filter trips with needed_reviews <= actual_reviews"""
-        for i in range(self.iters):
+        """Create reviews for random trips"""
+        for i in range(self.trips_count):
             trip = random.choices(trips)[0]
 
             review = Review(
@@ -61,23 +30,23 @@ class TripsForReviewTest(TestCase, BaseTest):
                 result=ReviewResult.ACCEPTED, result_comment="GOOD")
             review.save()
 
-            actual_reviews[trip.id] += 1
-            needed_reviewers_count = get_reviewers_count_by_difficulty(trip.difficulty_category)
-            if actual_reviews[trip.id] == needed_reviewers_count:
-                should_filtered += 1
-
-        """Filter trips with needed_reviews <= actual_reviews + reviews_in_work"""
-        for i in range(self.iters):
+        """Take random trips in work"""
+        for i in range(self.trips_count):
             trip = random.choices(trips)[0]
-
             TripsOnReviewByUser(user=self.leader, trip=trip).save()
+
+        should_filtered = 0
+
+        for trip in trips:
             needed_reviews_count = get_reviewers_count_by_difficulty(trip.difficulty_category)
-            actual_reviews[trip.id] += 1
-            if actual_reviews[trip.id] == needed_reviews_count:
+            in_work_reviews = len(TripsOnReviewByUser.objects.filter(trip=trip))
+            actual_reviews = len(Review.objects.filter(trip=trip))
+
+            if actual_reviews + in_work_reviews >= needed_reviews_count:
                 should_filtered += 1
 
-        trips = self.client.get('/api/trips/for-review').data
-        self.assertEqual(len(trips), self.iters - should_filtered)
+        trips_for_review = len(self.client.get('/api/trips/for-review').data)
+        self.assertEqual(trips_for_review, self.trips_count - should_filtered)
 
 
 class ReviewCreateTest(TestCase, BaseTest):
