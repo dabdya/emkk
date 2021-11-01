bcc31ab765bdc7962dee9d7a521b1d3d60598bfb
Сергей Пищулов <57267495+SergeyPishchulov@users.noreply.github.com>
2021-11-01 14:32:36 +0500

    Delete redundant setdefault

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Base')
 
 from django.core.wsgi import get_wsgi_application
