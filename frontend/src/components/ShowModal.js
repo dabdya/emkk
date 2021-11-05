5b5ab12f1050ea279b64c709dc2b119429d95544
mistralook <mistralook@gmail.com>
2021-11-05 21:16:29 +0500

    modal

---
+++ frontend/src/components/ShowModal.js
@@ -0,0 +1,22 @@
+import {Button, Modal} from "@skbkontur/react-ui";
+import React from "react";
+
+export default class ShowModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <Modal onClose={this.props.close}>
+                <Modal.Header>ЭМКК</Modal.Header>
+                <Modal.Body>
+                    <p>Заявка была подана!</p>
+                </Modal.Body>
+                <Modal.Footer panel={true}>
+                    <Button onClick={this.props.close}>Закрыть</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+}
