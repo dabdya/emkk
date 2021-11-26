516aa5c215dfb56d6733525b1673937f6875ca86
dabdya <iamdabdya@gmail.com>
2021-11-26 12:28:06 +0500

    Add permission for review

--- backend/src/emkk_site/urls.py
+++ backend/src/emkk_site/urls.py
@@ -2,18 +2,21 @@ from django.urls import path
 
 from src.emkk_site.views import (
     DocumentList, DocumentDetail,
-    ReviewerView, IssuerView, ReviewDetail,
+    ReviewerList, IssuerList,
     TripList, TripDetail, WorkRegisterView, )
 
+
 urlpatterns = [
+
     path('trips', TripList.as_view()),
     path('trips/<int:pk>', TripDetail.as_view()),
-    path('trips/<int:pk>/documents', DocumentList.as_view()),
 
-    path('trips/<int:pk>/reviews', ReviewerView.as_view()),
-    path('trips/<int:pk>/reviews/<int:rev_id>', ReviewDetail.as_view()),
-    path('trips/<int:pk>/reviews-from-issuer', IssuerView.as_view()),
+    path('trips/<int:pk>/documents', DocumentList.as_view()),
+    path('documents/<uuid:doc_uuid>', DocumentDetail.as_view()),
+
+    path('trips/<int:pk>/reviews', ReviewerList.as_view()),
+    path('trips/<int:pk>/reviews-from-issuer', IssuerList.as_view()),
+
     path('trips/work', WorkRegisterView.as_view()),
 
-    path('documents/<uuid:doc_uuid>', DocumentDetail.as_view()),
 ]
