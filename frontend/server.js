64fc87d4eed4f86e6bc50721b0b932db351bdeae
Knyazzini <knyazzev.lgor@gmail.com>
2021-12-17 18:33:16 +0500

    http -> https

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,28 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+var fs = require("fs");
+var path = require("path");
+var https = require("https");
+var privateKey = fs.readFileSync("./certs/cert.key", "utf8");
+var certificate = fs.readFileSync("./certs/cert.pem", "utf8");
 
-const PORT = 80;
-const BUILD_FOLDER = "build";
+var credentials = { key: privateKey, cert: certificate };
+var express = require("express");
+var app = express();
+
+const BUILD_FOLDER = "build"
 
 app.use(express.static(path.join(__dirname, BUILD_FOLDER)));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, BUILD_FOLDER, "index.html"));
+app.use(function (req, res, next) {
+	if (!req.secure) {
+		return res.redirect(["https://", req.get("Host"), req.url].join(""));
+	}
+	next();
 });
 
-app.listen(80);
+app.get("*", function (req, res) {
+	res.sendFile(path.join(__dirname, BUILD_FOLDER, "index.html"));
+});
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(443);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { HashRouter, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 
 ReactDOM.render(
